8061860f256695ca65a076e68581034de6d2a464
王汝鹏 <wangrupeng@live.cn>
2018-06-06 18:17:47 +0800

    hash

--- code/AutumnRecruitProject/src/test/java/com/wangrupeng/test/HashMapTest.java
+++ code/AutumnRecruitProject/src/test/java/com/wangrupeng/test/HashMapTest.java
@@ -9,10 +9,19 @@ import java.util.concurrent.ConcurrentHashMap;
 public class HashMapTest {
     public static void main(String[] args) {
         HashMap<Integer, String> hashMap = new HashMap<>();
-        
+
         LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>();
         TreeMap<Integer, String> treeMap = new TreeMap<>();
         ConcurrentHashMap<Integer, String> concurrentHashMap = new ConcurrentHashMap<>();
         Hashtable<Integer, String> hashtable = new Hashtable<>();
+        String str = "1231";
+        System.out.println(str.hashCode() >>> 16);
+        int h = hash(str);
+        System.out.println(hash(str));
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
 }

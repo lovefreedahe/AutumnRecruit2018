283f631a0d519bef92b588dc8cb8f99be42c8186
王汝鹏 <wangrupeng@live.cn>
2018-07-07 11:49:49 +0800

    d

---
+++ articles/distribution.md
@@ -0,0 +1,3 @@
+# raft协议
+
+[raft动画演示](http://thesecretlivesofdata.com/raft/)
--- code/AutumnRecruitProject/src/main/java/com/wangrupeng/jianzhioffer/question4/ReplaceSpace.java
+++ code/AutumnRecruitProject/src/main/java/com/wangrupeng/jianzhioffer/question4/ReplaceSpace.java
@@ -1,11 +1,15 @@
+package com.wangrupeng.jianzhioffer.question4;
 
 public class ReplaceSpace {
-    public ReplaceSpace() {
-
+    public String replace(String str) {
+        String result = str.replaceAll(" ", "%20");
+        return result;
     }
 
-    public String replaceSpace(String str) {
-        String result = "";
-        
+    public static void main(String[] args) {
+        ReplaceSpace replaceSpace = new ReplaceSpace();
+        String str = "What a beautiful world!";
+        String result = replaceSpace.replace(str);
+        System.out.println(result);
     }
 }

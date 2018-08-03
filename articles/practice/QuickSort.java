59880f6c10a1b63ddff2c693ade04ed701939226
王汝鹏 <wangrupeng@live.cn>
2018-08-03 12:15:31 +0800

    111

--- articles/practice/QuickSort.java
+++ articles/practice/QuickSort.java
@@ -1,3 +1,4 @@
+
 import java.util.Stack;
 public class QuickSort {
 
@@ -17,27 +18,33 @@ public class QuickSort {
         }
     }
 
-
     private void quickSortNonRecursive(int[] array, int start, int end) {
         if(start < end) {
             Stack<Record> stack = new Stack<>();
             int position = partition(array, start, end);
-            stack.push(new Record(start, position - 1));
-            stack.push(new Record(position + 1, stack));
+            if(start <= position - 1) {
+                stack.put(new Record(start, position - 1));
+            }
+            if(end >= position + 1) {
+                stack.put(new Record(position + 1, end));
+            }
             while(!stack.isEmpty()) {
                 Record record = stack.pop();
                 position = partition(array, record.left, record.right);
-                stack.push(array, record.left, position - 1);
-                stack.push(array, position + 1, record.right);
+                if(record.left <= position - 1) {
+                    partition(array, record.left, position - 1);
+                }
+                if(record.right >= position + 1) {
+                    partition(array, position + 1, record.right);
+                }
             }
         }
     }
 
-    private void partition(int[] array, int start, int end) {
+    private int partition(int[] array, int start, int end) {
         int right = end;
         int mark = array[end];
-
-        while(start <end) {
+        while(start < end) {
             while(start < end && array[start] <= mark) {
                 ++start;
             }
@@ -47,13 +54,13 @@ public class QuickSort {
             if(start < end) {
                 swap(array, start, end);
             }
-        }    
-
+        }
         if(right != end) {
             swap(array, right, end);
         }
         return end;
     }
+
 
     private void swap(int[] array, int a, int b) {
         int temp = array[a];
@@ -62,30 +69,29 @@ public class QuickSort {
     }
 
     private class Record {
-        int left;
-        int right;
+        private int left;
+        private int right;
         private Record(int left, int right) {
             this.left = left;
             this.right = right;
         }
     }
 
-    public void printArray(int[] array) {
-        for(int i:array) {
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        int[] array = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+        quickSort.sort(array);
+        System.out.println("Quick sort with recursive:");
+        for(int i : array) {
             System.out.print(i + " ");
         }
         System.out.println("\n");
+        array = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+        quickSort.sortNonRecursive(array);
+        System.out.println("Quick sort with non-recursive:");
+        for(int i : array) {
+            System.out.print(i + " ");
+        }
+        
     }
-
-    public static void main(String[] args) {
-        QuickSort quickSort = new QuickSort();
-        int[] array = {3, 1, 2, 6, 4, 2};
-        int[] array2 = {3, 1, 2, 6, 4, 2};
-        quickSort.sort(array);
-        System.out.println("Quick sort with recursive:");
-        quickSort.printArray(array);
-        quickSort.sortNonRecursive(array2);
-        quickSort.printArray(array2);
-    }
-
 }

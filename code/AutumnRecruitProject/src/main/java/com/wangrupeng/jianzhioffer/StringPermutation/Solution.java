9c42fd5e8ce3380e85cb9e97b351aa3b727d2c56
王汝鹏 <wangrupeng@live.cn>
2018-08-22 23:48:43 +0800

    123

--- code/AutumnRecruitProject/src/main/java/com/wangrupeng/jianzhioffer/StringPermutation/Solution.java
+++ code/AutumnRecruitProject/src/main/java/com/wangrupeng/jianzhioffer/StringPermutation/Solution.java
@@ -1,6 +1,9 @@
 package com.wangrupeng.jianzhioffer.StringPermutation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Solution {
     private ArrayList<String> list = new ArrayList<>();
@@ -11,11 +14,51 @@ public class Solution {
         if (str.equals("")) {
             return list;
         }
-        Permutation(str, 0, str.length() - 1);
-        return list;
+        if (str.length() == 1) {
+            list.add(str);
+            return list;
+        }
+        PermutationRecursive(str.toCharArray(), 0);
+        return new ArrayList<>(new TreeSet<>(list));
     }
 
-    private void Permutation(String str, int start, int end) {
+    private void PermutationRecursive(char[] chars, int position) {
+        if (position + 1 >= chars.length) {
+            list.add(String.valueOf(chars));
+            return;
+        }
+        //list.add(String.copyValueOf(chars));
+        for (int i = position;i < chars.length;i++) {
+            swap(chars, position, i);
+            PermutationRecursive(chars, position + 1);
+            swap(chars, position, i);
+        }
+    }
+
+    private void swap(char[] chars, int a, int b) {
+        if (a == b) {
+            return;
+        }
+        char temp = chars[a];
+        chars[a] = chars[b];
+        chars[b] = temp;
+    }
+
+    private char[] copyChars(char[] chars) {
+        char[] temp = new char[chars.length];
+        for (int i = 0;i < chars.length;i++) {
+            temp[i] = chars[i];
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        String test = "aabc";
+        Solution solution = new Solution();
+        ArrayList<String> list = solution.Permutation(test);
+        for (String str : list) {
+            System.out.println(str);
+        }
 
     }
 }

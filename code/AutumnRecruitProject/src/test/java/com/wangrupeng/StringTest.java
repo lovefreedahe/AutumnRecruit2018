67f14384030f1d173d1c10f30788223fc6d332bc
王汝鹏 <wangrupeng@live.cn>
2018-08-26 22:53:50 +0800

    2018.8.26

--- code/AutumnRecruitProject/src/test/java/com/wangrupeng/StringTest.java
+++ code/AutumnRecruitProject/src/test/java/com/wangrupeng/StringTest.java
@@ -1,24 +1,27 @@
 package com.wangrupeng;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class StringTest {
     public static void main(String[] args) {
-        String test = "helloworld";
+        String test = "helloworlddghfghfjh";
         /*StringBuilder builder = new StringBuilder(test);
         test = builder.reverse().toString();*/
         System.out.println(test);
         System.out.println("worldhello");
-        /*10
-        0
-        5 3 0
-        8 4 0
-        9 0
-        9 0
-        3 0
-        0
-        7 9 0
-        0
-        9 7 0*/
+        HashMap<String, Integer> map = new HashMap<>(3);
+        map.put(test, null);
+        int hashCode = hash(test);
+        System.out.println(hashCode);
+        int index = (4 - 1) & hashCode;
+        System.out.println(index);
+    }
+
+    static final int hash(Object key) {
+        int h;
+        int temp = key.hashCode() >>> 16;
+        System.out.println(temp);
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
 }
